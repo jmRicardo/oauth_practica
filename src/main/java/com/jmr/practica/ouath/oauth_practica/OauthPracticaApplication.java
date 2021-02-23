9b3af64f447ef68038f7f535e8010b90e2611068
jmRicardo <51313148+jmRicardo@users.noreply.github.com>
2021-02-23 15:03:18 -0300

    cargadas dependencias para administrar seguridad en los servicios

---
+++ src/main/java/com/jmr/practica/ouath/oauth_practica/OauthPracticaApplication.java
@@ -0,0 +1,13 @@
+package com.jmr.practica.ouath.oauth_practica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthPracticaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthPracticaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jmr/practica/ouath/oauth_practica/OauthPracticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmr.practica.ouath.oauth_practica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthPracticaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
